eac612736c1fd2f28e104c3e9cbf3fa942f479fc
Maddie Gordon <maddie@Madelines-MacBook-Pro-2.local>
2016-10-03 10:33:44 -0700

    experiments.java updated

--- Assignment5/src/ml/utils/Experiments.java
+++ Assignment5/src/ml/utils/Experiments.java
@@ -9,7 +9,8 @@ public class Experiments {
 		String wineFile = "/Users/maddie/Documents/FALL2016/MachineLearning/hw5/wines.train.txt";
 		DataSet wineDataset = new DataSet(wineFile, DataSet.TEXTFILE);
 		
-		DecisionTreeClassifier dt = new DecisionTreeClassifier();
+		//Q1
+//		DecisionTreeClassifier dt = new DecisionTreeClassifier();
 		//dt.setDepthLimit(5);
 		//dt.train(wineDataset);
 		//System.out.println(dt.toString());
@@ -21,44 +22,48 @@ public class Experiments {
 //			}	
 //		}
 		
-//		DataSetSplit ds = wineDataset.split(0.8);
-//		for(int i = 0; i < 50; i++) {
-//			dt.setDepthLimit(i);
-//			dt.train(ds.getTrain());
-//			System.out.println("Depth = " + i);
-//			System.out.println("Train acc: " + getAccuracy(dt, ds.getTrain(), wineDataset));
-//			System.out.println("Test acc: " + getAccuracy(dt, ds.getTest(), wineDataset) + "\n");
-//		}
+		//Q3
+		DataSetSplit ds = wineDataset.split(0.8);
+		for(int i = 0; i < 50; i++) {
+			DecisionTreeClassifier dt = new DecisionTreeClassifier();
+			dt.setDepthLimit(i);
+			dt.train(ds.getTrain());
+			System.out.println("Depth = " + i);
+			System.out.println("Train acc: " + getAccuracy(dt, ds.getTrain(), wineDataset));
+			System.out.println("Test acc: " + getAccuracy(dt, ds.getTest(), wineDataset) + "\n");
+		}
 		
 		//Q4 
 		
-		CrossValidationSet cvs = new CrossValidationSet(wineDataset, 10, true);
+		/*CrossValidationSet cvs = new CrossValidationSet(wineDataset, 10, true);
 		for (int i = 0; i < 10; i++) {
-			DataSetSplit ds = cvs.getValidationSet(i);
+			DataSetSplit dss = cvs.getValidationSet(i);
 			System.out.print(i + ",");
+			
 			for(int d = 1; d < 4; d++) {
 				ClassifierFactory factory = new ClassifierFactory(0);
 				OVAClassifier oc1 = new OVAClassifier(factory); //dt classifier
 				AVAClassifier ac = new AVAClassifier(factory);
 				DecisionTreeClassifier dtc = (DecisionTreeClassifier) factory.getClassifier(); //get a new DTC
 				dt.setDepthLimit(d);
-				oc1.train(ds.getTrain());
-				ac.train(ds.getTrain());
-				
-			}
-		}
-		
-		
-		
+				oc1.train(dss.getTrain());
+				ac.train(dss.getTrain());
+				double ocAcc = getAccuracy(oc1, dss.getTest(), wineDataset);
+				double acAcc = getAccuracy(ac, dss.getTest(), wineDataset);
+				System.out.println(d + ", " + ocAcc + ", " + acAcc);
+			}	
+		}*/
+			
 	}
-	public static double getAccuracy(DecisionTreeClassifier dt, DataSet ds, DataSet examples ) {
+	
+	public static double getAccuracy(Classifier dt, DataSet ds, DataSet examples ) {
 		double acc = 0.0;
-		for(Example ex: examples.getData()) {
+		for(Example ex: ds.getData()) {
 			if(ex.getLabel() == dt.classify(ex)) {
-				acc += 1.0/(double)examples.getData().size();
+				acc += 1.0; //(double)examples.getData().size();
 			}
 		}
-		return acc;
+		return acc/(double)ds.getData().size();
 	}
 		
 }
